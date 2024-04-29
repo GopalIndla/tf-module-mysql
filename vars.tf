9aa6dbe2561b9e3f734f87863855ca2ef3391b4a
CodingManoj <CodingManoj@gmail.com>
2024-04-28 21:51:31 -0400

    Auto Push

--- vars.tf
+++ vars.tf
@@ -5,4 +5,4 @@ variable "MYSQL_ENGINE_VERSION" {}
 variable "MYSQL_INSTANCE_TYPE" {}
 variable "MYSQL_SKIP_SNAPSHOT" {}
 variable "MYSQL_PORT" {}
-variable "MYSQL_ENGINE_FAMILY {}
+variable "MYSQL_ENGINE_FAMILY" {}
