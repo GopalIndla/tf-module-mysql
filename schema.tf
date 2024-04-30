3ea01bc26c93812e3388cd3172ed19a278a02552
CodingManoj <CodingManoj@gmail.com>
2024-04-29 21:21:32 -0400

    Auto Push

--- schema.tf
+++ schema.tf
@@ -13,3 +13,12 @@ resource "null_resource" "schema" {
         EOF
     }
 }
+
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z08185973US3IG8LL97B8"
+  name    = "mysql-${var.ENV}.roboshop.internal"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_db_instance.mysql.address]
+}
